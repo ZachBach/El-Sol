d6639d9957a19990373c5e521935447e88b61a7b
Zach Auerbach <auernachzach@gmail.com>
2021-03-08 11:45:02 -0800

    rotation of shaders is not working properly however it is rendering the vUv's not sure what broke it

--- js/shaderSun/vertex.glsl
+++ js/shaderSun/vertex.glsl
@@ -3,7 +3,34 @@ varying vec2 vUv;
 varying vec3 vPosition;
 float PI=3.141592653589793238;
 
+varying vec3 vLayer0;
+varying vec3 vLayer1;
+varying vec3 vLayer2;
+
+mat2 rotate(float a){
+  float s=sin(a);
+  float c=cos(a);
+  return mat2(c,-s,s,c);
+}
+
 void main(){
+  
+  float t=time*.5;
+  
+  mat2 rot=rotate(t);
+  
+  vec3 p0=position;
+  p0.yz=rot*p0.yz;
+  vLayer0=p0;
+  
+  vec3 p1=position;
+  p1.xz=rot*p1.xz;
+  vLayer1=p1;
+  
+  vec3 p2=position;
+  p2.xy=rot*p2.xy;
+  vLayer2=p2;
+  
   vUv=uv;
   vPosition=position;
   gl_Position=projectionMatrix*modelViewMatrix*vec4(position,1.);
